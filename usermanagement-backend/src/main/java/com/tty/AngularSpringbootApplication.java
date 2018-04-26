ad3dd3205e964cae15f216d736711285316b085c
Vivek Singh <vivekk.singh03@gmail.com>
2018-04-26 01:20:45 -0400

    Base Code with working Maven Multimodule - SpringBoot & Angular

---
+++ usermanagement-backend/src/main/java/com/tty/AngularSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootApplication.class, args);
+	}
+}
